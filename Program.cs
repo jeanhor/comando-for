c65ce5d38cf591e4bc9e29ee6c4f6b78a3861dc3
Jean Carlos Horacio <Jean.horacio@intranet.local>
2021-06-18 14:18:35 -0300

    Adicionado console.readline

--- Program.cs
+++ Program.cs
@@ -6,8 +6,12 @@ namespace comando_for
     {
         static void Main(string[] args)
         {
-            int temperatura;
-            for ( temperatura = 1; temperatura <= 18;temperatura++ );
+            int temperatura = 0;
+            Console.WriteLine("Digite uma temperatura");//Adicionado 18/06/2021
+            string numero = Console.ReadLine();
+            Console.WriteLine ("Voce digitou " +numero );
+            
+            for ( temperatura = 0; temperatura <= 18;temperatura++ );
                   Console.WriteLine(temperatura);
                  
         }
