0e14c96c17a97999fff15664f21fd56424925619
Jean Carlos Horacio <Jean.horacio@intranet.local>
2021-06-18 12:22:09 -0300

    criacao comando for

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace comando_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperatura;
+            for ( temperatura = 1; temperatura <= 18;temperatura++ );
+                  Console.WriteLine(temperatura);
+                 
+        }
+    }
+}
